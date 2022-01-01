a555b54f3158d0f6ff697bc77e5608a792a43f46
unknown <jason2133@likelion.org>
2022-01-01 15:08:36 +0900

    HTTP Exception

--- fastapi/fastapienv/books2.py
+++ fastapi/fastapienv/books2.py
@@ -1,7 +1,14 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request, status, Form, Header
 from pydantic import BaseModel, Field
 from uuid import UUID
+from starlette.responses import JSONResponse
+
+
+class NegativeNumberException(Exception):
+    def __init__(self, books_to_return):
+        self.books_to_return = books_to_return
+
 
 app = FastAPI()
 
@@ -36,11 +43,57 @@ class Book(BaseModel):
         }
 
 
+class BookNoRating(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str
+    description: Optional[str] = Field(
+        None,
+        title="description of the Book",
+        max_length=100,
+        min_length=1
+    )
+
+
 BOOKS = []
+
+# Creating Custom Handlers is extremely important with an API,
+# so you can clarify the air back to the client and user.
+
+
+@app.exception_handler(NegativeNumberException)
+async def negative_number_exception_handler(request: Request,
+                                            exception: NegativeNumberException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Hey, why do you want {exception.books_to_return} "
+                            f"books? You need to read more!"}
+    )
+
+# Form Fields is different than query parameters and JSON and is typically sent using the HTML form tags.
+# Many different types of authentication platforms want the username and password to be sent using forms and not a different type of data transfer.
+# When using form data, the data is typically encoded using the media type.
+# In FastAPI will automatically decode the forms for you.
+
+
+@app.post("/books/login")
+async def book_login(username: str = Form(...), password: str = Form(...)):
+    return {"username": username, "password": password}
+
+
+# Headers are a way that we can send additional information with each request
+# As the API receives the headers, we can now do whatever we want the headers and do some kind of validation if needed
+@app.get('/header')
+async def read_header(random_header: Optional[str] = Header(None)):
+    return {"Random-Header": random_header}
 
 
 @app.get("/")
 async def read_all_books(books_to_return: Optional[int] = None):
+
+    if books_to_return and books_to_return < 0:
+        raise NegativeNumberException(books_to_return=books_to_return)
+
     if len(BOOKS) < 1:
         create_books_no_api()
 
@@ -61,11 +114,21 @@ async def read_book(book_id: UUID):
     for x in BOOKS:
         if x.id == book_id:
             return x
+    raise raise_item_cannot_be_found_exception()
+
+
+@app.get('/book/rating/{book_id}', response_model=BookNoRating)
+async def read_book_no_rating(book_id: UUID):
+    for x in BOOKS:
+        if x.id == book_id:
+            return x
+    raise raise_item_cannot_be_found_exception()
 
 # Post Request BaseModel
+# Status Code Response : status_code=status.HTTP_201_CREATED
 
 
-@app.post("/")
+@app.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book: Book):
     BOOKS.append(book)
     return book
@@ -79,6 +142,7 @@ async def update_book(book_id: UUID, book: Book):
         if x.id == book_id:
             BOOKS[counter - 1] = book
             return BOOKS[counter - 1]
+    raise raise_item_cannot_be_found_exception()
 
 
 @app.delete("/{book_id}")
@@ -89,6 +153,10 @@ async def delete_book(book_id: UUID):
         if x.id == book_id:
             del BOOKS[counter - 1]
             return f"ID: {book_id} deleted"
+    raise raise_item_cannot_be_found_exception()
+    # raise HTTPException(status_code=404, detail='Book not found',
+    #                     headers={'X-Header-Error':
+    #                              "Nothing to be seen at the UUID"})
 
 
 def create_books_no_api():
@@ -116,3 +184,10 @@ def create_books_no_api():
     BOOKS.append(book_2)
     BOOKS.append(book_3)
     BOOKS.append(book_4)
+
+
+def raise_item_cannot_be_found_exception():
+    return HTTPException(status_code=404,
+                         detail='Book not found',
+                         headers={'X-Header_Error':
+                                  "Nothing to be seen at the UUID"})
