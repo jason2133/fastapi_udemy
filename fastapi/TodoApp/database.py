e41cbf9cec26d580599d3c4d628021f5fa15a279
unknown <jason2133@likelion.org>
2022-01-03 23:35:04 +0900

    routing

--- fastapi/TodoApp/database.py
+++ fastapi/TodoApp/database.py
@@ -4,11 +4,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://test1234!@127.0.0.1:3306/todoapp"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={'check_same_thread': False}
+    SQLALCHEMY_DATABASE_URL
+    # connect_args={'check_same_thread': False}
 )
 
 # Create a Database Session
